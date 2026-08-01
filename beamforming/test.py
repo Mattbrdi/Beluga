608fb9c41922eeef1c4320b69123e2f858591a8f
supercasamario2 <amine1521996@gmail.com>
2026-07-31 20:25:36 -0400

    Test changes

--- beamforming/test.py
+++ beamforming/test.py
@@ -45,9 +45,10 @@ print("Beamforming Doa processing")
 
 # plot_results(power_DB, Theta, Phi)
 
-# u = mvdr_doa(2000, tetrahedra, audio_array)
+u = mvdr_doa(2000, tetrahedra, audio_array)
 # u = delay_and_sum_doa(2000, tetrahedra, audio_array)
-u = music_doa(2000, tetrahedra, audio_array, num_expected_signals=1)
+# u = music_doa(2000, tetrahedra, audio_array, num_expected_signals=1)
+
 print(u)
 print("Visualize result")
 
